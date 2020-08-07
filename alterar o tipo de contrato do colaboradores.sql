b5570b720c7d777356f9d478eadf1d6e2cf031fd
fabiobrunofbi <47822260+fabiobrunofbi@users.noreply.github.com>
2020-08-07 07:52:52 -0300

    Add files via upload

---
+++ alterar o tipo de contrato do colaboradores.sql
@@ -0,0 +1,10 @@
+SELECT *
+  FROM r034fun 
+  --update r034fun set outcon = 'U'
+ WHERE numemp = 4 
+   AND tipcol = 1 
+   and outcon = '0'
+   and datadm >= '08/01/2018'
+   AND numcad = 11550
+
+
